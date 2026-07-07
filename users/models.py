ebee165d1136aadb0cba3f4020e16f2cdaf174b4
Talha Ansar <talhabajwa.x1@gmail.com>
2026-07-08 01:02:57 +0500

    initial user model created and migration done

--- users/models.py
+++ users/models.py
@@ -7,5 +7,3 @@ class User(AbstractUser):
     phone_number = models.CharField(max_length=20, unique=True)
     date_of_birth = models.DateField(null=True, blank=True)
     country = models.CharField(max_length=100, null=True, blank=True)
-   
-    pass
