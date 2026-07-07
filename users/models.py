69517cc9e80ec9dccf6f91dda23aee7ce55a4526
Talha Ansar <talhabajwa.x1@gmail.com>
2026-07-07 23:52:12 +0500

    working on users

---
+++ users/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class User(AbstractUser):
+    
+    phone_number = models.CharField(max_length=20, unique=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    country = models.CharField(max_length=100, null=True, blank=True)
+   
+    pass
