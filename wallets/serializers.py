2b99833e01b95e911e2a7b47e886d8bf06cee8c9
Talha Ansar <talhabajwa.x1@gmail.com>
2026-07-12 23:24:52 +0500

    some fixes after testing the endpoint

--- wallets/serializers.py
+++ wallets/serializers.py
@@ -4,5 +4,5 @@ from rest_framework import serializers
 class WalletSerializer(serializers.ModelSerializer):
     class Meta:
         model = Wallet
-        fields = ['id', 'user', 'status', 'currency', 'created_at', 'updated_at']
+        fields = ['id', 'user', 'status', 'currency', 'balance','created_at', 'updated_at']
         read_only_fields = ['id', 'user', 'balance', 'created_at', 'updated_at']
