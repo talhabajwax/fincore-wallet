32d53e73e6a664fdcf06e60b6f0fe8ee8402a001
Talha Ansar <talhabajwa.x1@gmail.com>
2026-07-14 02:54:51 +0500

    migrations completed

--- ledger/models.py
+++ ledger/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
-class ledgerAccount(models.Model):
+class LedgerAccount(models.Model):
         wallet = models.OneToOneField(
         "wallets.Wallet",
         on_delete=models.CASCADE,
         related_name="ledger_account",)
+        created_at = models.DateTimeField(auto_now_add=True)
+        updated_at = models.DateTimeField(auto_now=True)
         
