eca96a79d40f80362a0e133ce483226912b9a97a
Talha Ansar <talhabajwa.x1@gmail.com>
2026-07-15 03:22:53 +0500

    model created for ledger entry and migrations done

--- ledger/models.py
+++ ledger/models.py
@@ -8,3 +8,10 @@ class LedgerAccount(models.Model):
         created_at = models.DateTimeField(auto_now_add=True)
         updated_at = models.DateTimeField(auto_now=True)
         
+class LedgerEntry(models.Model):
+        ENTRY_TYPES = [("credit","Credit"),("debit","Debit")]
+        transaction = models.ForeignKey("transactions.Transaction",on_delete=models.PROTECT,related_name="transactions_entries",)
+        ledger_account = models.ForeignKey("ledger.LedgerAccount",on_delete=models.PROTECT,related_name="ledger_entries",)
+        entry_type = models.CharField(max_length=20,choices=ENTRY_TYPES,)
+        amount = models.DecimalField(max_digits=12,decimal_places=2,)
+        created_at = models.DateTimeField(auto_now_add=True,)
