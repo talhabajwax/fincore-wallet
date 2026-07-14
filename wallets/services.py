8da6c0951d7366fe100102cb44b0b655c8c0d9e1
Talha Ansar <talhabajwa.x1@gmail.com>
2026-07-14 18:34:00 +0500

    ledger service connected to wallet service

--- wallets/services.py
+++ wallets/services.py
@@ -1,12 +1,19 @@
 from .repositeries import WalletRepository
+from ledger.services import LedgerService
+from django.db import transaction
 
 class WalletService:
     
+    @transaction.atomic
     def create_wallet(self, user, currency="PKR"):
         wallet_repo = WalletRepository()
+        
         if wallet_repo.wallet_exists(user, currency):
             raise ValueError("Wallet already exists for this user and currency.")
-        return wallet_repo.create_wallet(user, currency)
+        wallet = wallet_repo.create_wallet(user, currency)
+        ledger_service = LedgerService()
+        ledger_service.create_ledger(wallet)
+        return wallet
     
     def all_wallets(self, user):
         all_wallets_repo= WalletRepository()
