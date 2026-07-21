10dd118ac8a38c0e15a67e44d2ee5cd4f56fc359
Talha Ansar <talhabajwa.x1@gmail.com>
2026-07-22 03:14:00 +0500

    starting working on idempotency and idempotency model created

--- transactions/models.py
+++ transactions/models.py
@@ -73,3 +73,31 @@ class Transfer(models.Model):
         on_delete=models.PROTECT,
         related_name="received_transfers",
     )
+    
+class IdempotencyRecord(models.Model):
+    user = models.ForeignKey(
+    "users.User",
+    on_delete=models.PROTECT,
+    related_name="idempotency_records",)
+    key = models.CharField(max_length=255)
+    OPERATION_TYPES = [
+    ("transfer", "Transfer"),
+    ("deposit", "Deposit"),
+    ("withdrawal", "Withdrawal"),
+    ("reversal", "Reversal"),]
+    operation_type = models.CharField(max_length=20, choices=OPERATION_TYPES)
+    request_fingerprint = models.CharField(max_length=64)
+    STATUS_CHOICES = [
+    ("processing", "Processing"),
+    ("completed", "Completed"),
+    ("failed", "Failed"),]
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="processing")
+    transaction = models.ForeignKey(
+    "transactions.Transaction",
+    on_delete=models.PROTECT,
+    related_name="idempotency_records",
+    null=True,
+    blank=True,)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
