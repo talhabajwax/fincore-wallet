2e358cbd8c155f1218a041802ca9d639cda7f3b8
Talha Ansar <talhabajwa.x1@gmail.com>
2026-07-29 03:07:32 +0500

    urls added for freeze wallet

--- wallets/urls.py
+++ wallets/urls.py
@@ -1,9 +1,25 @@
 from django.urls import path
 
-from .views import AllWalletsView, CreateWalletView,AWalletView
+from .views import (
+    AllWalletsView,
+    AWalletView,
+    CreateWalletView,
+    FreezeWalletView,
+    UnfreezeWalletView,
+)
 
 urlpatterns = [
     path("create/", CreateWalletView.as_view(), name="create-wallet"),
     path("", AllWalletsView.as_view(), name="All-Wallets"),
     path("<int:wallet_id>/", AWalletView.as_view(), name="a-wallet"),
+    path(
+        "<int:wallet_id>/freeze/",
+        FreezeWalletView.as_view(),
+        name="freeze-wallet",
+    ),
+    path(
+        "<int:wallet_id>/unfreeze/",
+        UnfreezeWalletView.as_view(),
+        name="unfreeze-wallet",
+    ),
 ]
--- wallets/views.py
+++ wallets/views.py
@@ -1,7 +1,7 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .serializers import WalletSerializer
 from .services import WalletService
@@ -14,10 +14,7 @@ class CreateWalletView(APIView):
         serializer = WalletSerializer(data=request.data)
 
         if not serializer.is_valid():
-            return Response(
-                serializer.errors,
-                status=status.HTTP_400_BAD_REQUEST
-            )
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
         user = request.user
         currency = serializer.validated_data["currency"]
@@ -26,86 +23,75 @@ class CreateWalletView(APIView):
         try:
             wallet = wallet_service.create_wallet(user, currency)
         except ValueError as error:
-            return Response(
-                {"error": str(error)},
-                status=status.HTTP_400_BAD_REQUEST
-            )
+            return Response({"error": str(error)}, status=status.HTTP_400_BAD_REQUEST)
 
         return Response(
-            {"message": "Wallet created successfully."},
-            status=status.HTTP_201_CREATED
+            {"message": "Wallet created successfully."}, status=status.HTTP_201_CREATED
         )
-        
+
+
 class AllWalletsView(APIView):
-    permission_classes = [IsAuthenticated]        
-        
-    def get (self ,request):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
         wallets_service = WalletService()
-        user=request.user
-        wallets=wallets_service.all_wallets(user)
+        user = request.user
+        wallets = wallets_service.all_wallets(user)
         serializer = WalletSerializer(wallets, many=True)
 
-        return Response(
-        serializer.data,
-        status=status.HTTP_200_OK)
-        
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
 class AWalletView(APIView):
     permission_classes = [IsAuthenticated]
-    def get (self,request,wallet_id):
-     user =request.user
-     wallet_service=WalletService()
-     wallet=wallet_service.a_wallet(user,wallet_id)
-     serializer=WalletSerializer(wallet)
-     if wallet is None:
+
+    def get(self, request, wallet_id):
+        user = request.user
+        wallet_service = WalletService()
+        wallet = wallet_service.a_wallet(user, wallet_id)
+        serializer = WalletSerializer(wallet)
+        if wallet is None:
             return Response(
-                {"error": "Wallet not found."},
-                status=status.HTTP_404_NOT_FOUND
+                {"error": "Wallet not found."}, status=status.HTTP_404_NOT_FOUND
             )
-     return Response(serializer.data,status=status.HTTP_200_OK)
- 
- 
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
 class FreezeWalletView(APIView):
     permission_classes = [IsAuthenticated]
-    def post(self,request,wallet_id):
-     user=request.user
-     if user.is_staff is False:
+
+    def post(self, request, wallet_id):
+        user = request.user
+        if user.is_staff is False:
+            return Response(
+                {"error": "Only staff members can freeze wallets."},
+                status=status.HTTP_403_FORBIDDEN,
+            )
+        wallet_service = WalletService()
+        try:
+            freezed_wallet = wallet_service.freeze_wallet(wallet_id)
+        except ValueError as error:
+            return Response({"error": str(error)}, status=status.HTTP_400_BAD_REQUEST)
         return Response(
-            {"error": "Only staff members can freeze wallets."},
-            status=status.HTTP_403_FORBIDDEN
+            {"message": "Wallet frozen successfully."}, status=status.HTTP_200_OK
         )
-     wallet_service=WalletService()
-     try:
-        freezed_wallet=wallet_service.freeze_wallet(wallet_id)
-     except ValueError as error:
-        return Response(
-            {"error": str(error)},
-            status=status.HTTP_400_BAD_REQUEST
-        )
-     return Response(
-        {"message": "Wallet frozen successfully."},
-        status=status.HTTP_200_OK
-    )
-    
+
+
 class UnfreezeWalletView(APIView):
     permission_classes = [IsAuthenticated]
-    def post(self,request,wallet_id):
-     user=request.user
-     if user.is_staff is False:
+
+    def post(self, request, wallet_id):
+        user = request.user
+        if user.is_staff is False:
+            return Response(
+                {"error": "Only staff members can unfreeze wallets."},
+                status=status.HTTP_403_FORBIDDEN,
+            )
+        wallet_service = WalletService()
+        try:
+            unfreezed_wallet = wallet_service.unfreeze_wallet(wallet_id)
+        except ValueError as error:
+            return Response({"error": str(error)}, status=status.HTTP_400_BAD_REQUEST)
         return Response(
-            {"error": "Only staff members can unfreeze wallets."},
-            status=status.HTTP_403_FORBIDDEN
+            {"message": "Wallet unfrozen successfully."}, status=status.HTTP_200_OK
         )
-     wallet_service=WalletService()
-     try:
-        unfreezed_wallet=wallet_service.unfreeze_wallet(wallet_id)
-     except ValueError as error:
-        return Response(
-            {"error": str(error)},
-            status=status.HTTP_400_BAD_REQUEST
-        )
-     return Response(
-        {"message": "Wallet unfrozen successfully."},
-        status=status.HTTP_200_OK
-    )
-     
-        
