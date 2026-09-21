0a463dc6d82d0853d9daca7fb1c5ecffd1cdb7d6
Talha Ansar <talhabajwa.x1@gmail.com>
2026-09-22 02:53:57 +0500

    audit model added

--- audit/models.py
+++ audit/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Audit(models.Model):
+    actor = models.ForeignKey(
+        "users.User",
+        on_delete=models.PROTECT,)
+    action = models.CharField(
+        max_length=50)
+    target_type = models.CharField(
+        max_length=50)
+    target_id = models.CharField(
+        max_length=100)
+    previous_state = models.JSONField(
+        null=True,
+        blank=True)
+    new_state = models.JSONField(
+        null=True,
+        blank=True)
+    reason = models.TextField(
+        null=True,
+        blank=True)
+    metadata = models.JSONField(
+        null=True,
+        blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
